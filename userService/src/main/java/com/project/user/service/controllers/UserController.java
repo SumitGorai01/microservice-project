2862e984e03651ed13dd4e4ef8bf9520522e4369
Sumit <goraisumit01@gmail.com>
2024-06-17 10:03:08 +0530

    implement circuit breaker using resilience 4j

--- userService/src/main/java/com/project/user/service/controllers/UserController.java
+++ userService/src/main/java/com/project/user/service/controllers/UserController.java
@@ -2,7 +2,10 @@ package com.project.user.service.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.support.FeignHttpClientProperties.OkHttp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,12 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.user.service.entities.User;
 import com.project.user.service.services.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	//create
 	@PostMapping("/")
 	public ResponseEntity<User> createUser(@RequestBody User user){
@@ -32,9 +40,24 @@ public class UserController {
 	
 	//get one user
 	@GetMapping("/{userId}")
+	@CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
 	public ResponseEntity<User> getOneUser(@PathVariable("userId") String userId){
+		logger.info("Get Single user Handler : UserController");
+
 		User user = userService.getUser(userId);
 		return ResponseEntity.ok(user);
+	}
+	
+	//create circuit breaker for fallback
+	public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+		logger.info("fallback is executed because Service is down : "+ex.getMessage());
+		User user = User.builder()
+					.email("dummy@gmail.com")
+					.name("dummy")
+					.about("This user is created because some service is down.")
+					.userId("1234567")
+					.build();
+		return new ResponseEntity<>(user,HttpStatus.OK);
 	}
 	//get all user
 	@GetMapping("/")
@@ -48,7 +71,6 @@ public class UserController {
 	//delete user
 	@DeleteMapping("/{userId}")
 	public ResponseEntity<?> deleteUser(@PathVariable String userId){
-		
 		userService.deleteUserById(userId);
 		
 		return ResponseEntity.ok().build();
